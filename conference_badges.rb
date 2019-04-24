003058c073076d0ca5e4ab50c5de13342d6ae2f6
Renee Nordholm <reneenordholm@gmail.com>
2019-04-24 18:58:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+    attendees.each do |names|
+      badges << badge_maker(names)
+  end
+ return badges
+end
+
+def assign_rooms(attendees)
+  counter = 1
+  room_assignments = []
+    until counter == 7
+      attendees.each_with_index do |names, index|
+        room_assignments << "Hello, #{names}! You'll be assigned to room #{index+1}!"
+    end
+      counter += 1
+     return room_assignments
+  end
+end
+
+def printer(attendees)
+  badges_and_room_assignments = []
+
+end
