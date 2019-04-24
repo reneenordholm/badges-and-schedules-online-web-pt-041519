067bac40d7b99ac9e31f66ffb203b833d94bd6b1
Renee Nordholm <reneenordholm@gmail.com>
2019-04-24 19:21:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,9 +23,8 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator[attendees]
-  assign_rooms[attendees]
+  batch_badge_creator(attendees).each {|data| puts data}
 end
 
 
-array.each {|item| puts "The current array item is: #{item}"}
+#array.each {|item| puts "The current array item is: #{item}"}
